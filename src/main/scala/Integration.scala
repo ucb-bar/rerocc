61751c3c23f6f7435cc264d32757956351cdb72a
Jerry Zhao <jerryz123@berkeley.edu>
2024-02-05 11:10:30 -0800

    Update to latest rocket-chip APIs

--- src/main/scala/Integration.scala
+++ src/main/scala/Integration.scala
@@ -34,7 +34,8 @@ trait CanHaveReRoCCTiles { this: BaseSubsystem with InstantiatesHierarchicalElem
     shouldBeInlined = false // can't inline something whose output we are are dontTouching
   )).node
   val reRoCCManagers = p(ReRoCCTileKey).zipWithIndex.map { case (g,i) =>
-    val rerocc_tile = LazyModule(new ReRoCCManagerTile(g.copy(reroccId = i), p))
+    val rerocc_prci_domain = locateTLBusWrapper(SBUS).generateSynchronousDomain.suggestName(s"rerocc_prci_domain_$i")
+    val rerocc_tile = rerocc_prci_domain { LazyModule(new ReRoCCManagerTile(g.copy(reroccId = i), p)) }
     println(s"ReRoCC Manager id $i is a ${rerocc_tile.rocc}")
     locateTLBusWrapper(SBUS).coupleFrom(s"port_named_rerocc_$i") {
       (_ :=* TLBuffer() :=* rerocc_tile.tlNode)
@@ -45,14 +46,17 @@ trait CanHaveReRoCCTiles { this: BaseSubsystem with InstantiatesHierarchicalElem
   require(!(reRoCCManagers.isEmpty ^ reRoCCClients.isEmpty))
 
   if (!reRoCCClients.isEmpty) {
-    val rerocc_bus = p(ReRoCCNoCKey).map { k =>
-      if (k.useGlobalNoC) {
-        globalNoCDomain { LazyModule(new ReRoCCGlobalNoC(k)) }
-      } else {
-        LazyModule(new ReRoCCNoC(k))
-      }
-    }.getOrElse(LazyModule(new ReRoCCXbar()))
-    reRoCCClients.foreach { case (t, c) => rerocc_bus.node := t { ReRoCCBuffer() := c.reRoCCNode } }
-    reRoCCManagers.foreach { m => m.reRoCCNode := rerocc_bus.node }
+    val rerocc_bus_domain = locateTLBusWrapper(SBUS).generateSynchronousDomain
+    rerocc_bus_domain {
+      val rerocc_bus = p(ReRoCCNoCKey).map { k =>
+        if (k.useGlobalNoC) {
+          globalNoCDomain { LazyModule(new ReRoCCGlobalNoC(k)) }
+        } else {
+          LazyModule(new ReRoCCNoC(k))
+        }
+      }.getOrElse(LazyModule(new ReRoCCXbar()))
+      reRoCCClients.foreach { case (t, c) => rerocc_bus.node := t { ReRoCCBuffer() := c.reRoCCNode } }
+      reRoCCManagers.foreach { m => m.reRoCCNode := rerocc_bus.node }
+    }
   }
 }
